46e7ebaf9884ddb4354ca6c7d2528aaa65904284
egortsaryk9 <egortsaryk@gmail.com>
2022-11-18 01:53:58 +0000

    (wip): moderation tabs

--- src/modules/moderation/components/PendingAssetsList/PendingAssetsList.vue
+++ src/modules/moderation/components/PendingAssetsList/PendingAssetsList.vue
@@ -1,31 +1,37 @@
 <template>
-  <nft-items-infinite-scroll
-    v-slot="{list}"
-    :filter="filter"
-    is-draft
-    class="py-6"
-  >
-    <ve-auto-grid
-      cols="1"
-      cols-sm="2"
-      cols-lg="4"
+  <ve-stack class="mt-4">
+    <div class="d-flex justify-end">
+  
+    </div>
+
+    <nft-items-infinite-scroll
+      v-slot="{list}"
+      :filter="filter"
+      is-draft
+      class="py-6"
     >
-      <nft-item-draft-moderation-card
-        v-for="asset in list"
-        :key="asset._id"
-        :nft-item-draft="asset"
-        title-attribute-key="nftItem.title"
-        :success-approve-message="$t('moderation.pendingAssets.approveSuccess')"
-        :success-decline-message="$t('moderation.pendingAssets.declineSuccess')"
-      />
-    </ve-auto-grid>
-  </nft-items-infinite-scroll>
+      <ve-auto-grid
+        cols="1"
+        cols-sm="2"
+        cols-lg="4"
+      >
+        <nft-item-draft-moderation-card
+          v-for="asset in list"
+          :key="asset._id"
+          :nft-item-draft="asset"
+          title-attribute-key="nftItem.title"
+          :success-approve-message="$t('moderation.pendingAssets.approveSuccess')"
+          :success-decline-message="$t('moderation.pendingAssets.declineSuccess')"
+        />
+      </ve-auto-grid>
+    </nft-items-infinite-scroll>
+  </ve-stack>
 </template>
 
 <script>
   import { NftItemMetadataDraftStatus } from '@/casimir-framework/vars';
   import { NftItemsInfiniteScroll, NftItemDraftModerationCard } from '@/casimir-framework/modules/nft-items';
-  import { VeAutoGrid } from '@/casimir-framework/vue-elements';
+  import { VeAutoGrid, VeStack } from '@/casimir-framework/vue-elements';
 
   export default {
     name: 'PendingAssetsList',
@@ -33,7 +39,8 @@
     components: {
       NftItemDraftModerationCard,
       NftItemsInfiniteScroll,
-      VeAutoGrid
+      VeAutoGrid,
+      VeStack
     },
 
     data() {
